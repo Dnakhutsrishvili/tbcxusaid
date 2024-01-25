859f50664fadd351b30a281abaf2ffad19d46922
davit <dtnakhutsrishvili@gmail.com>
2024-01-25 21:42:07 +0400

    added questions section

--- main.js
+++ main.js
@@ -48,45 +48,49 @@ const Questions={
         let questionParent=document.createElement("div");
         questionParent.className="question-parent";
 
-        let question=document.createElement("h3");
-        question.innerHTML="როგორ ხდება კურსებზე რეგისტრაცია და შერჩევა?"
-        let svgContainer=document.createElement("div");
-        svgContainer.className="svg-container";
-
 
         let svgCodeDown='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="arrowDown" d="M8.14644661,10.1464466 C8.34170876,9.95118446 8.65829124,9.95118446 8.85355339,10.1464466 L12.4989857,13.7981758 L16.1502401,10.1464466 C16.3455022,9.95118446 16.6620847,9.95118446 16.8573469,10.1464466 C17.052609,10.3417088 17.052609,10.6582912 16.8573469,10.8535534 L12.4989857,15.2123894 L8.14644661,10.8535534 C7.95118446,10.6582912 7.95118446,10.3417088 8.14644661,10.1464466 Z"></path></svg>'
         let svgCodUp='<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path class="arrowUp" d="M8.14644661,14.858836 C7.95118446,14.6635739 7.95118446,14.3469914 8.14644661,14.1517292 L12.4989857,9.79289322 L16.8573469,14.1517292 C17.052609,14.3469914 17.052609,14.6635739 16.8573469,14.858836 C16.6620847,15.0540981 16.3455022,15.0540981 16.1502401,14.858836 L12.4989857,11.2071068 L8.85355339,14.858836 C8.65829124,15.0540981 8.34170876,15.0540981 8.14644661,14.858836 Z"></path></svg>'
-
-        let answerContainer=document.createElement("div");
-        let answerSectionText=document.createElement("p");
-        let answerHeader=document.createElement("p");
-        let answerFooter=document.createElement("p");
-        answerHeader.innerHTML="კურსზე რეგისტრაციისთვის უნდა გაიარო რამდენიმე ეტაპი:";
-        answerSectionText.innerHTML="- პირველ ეტაპზე საჭიროა, შეავსო სასურველი კურსისთვის განკუთვნილი სარეგისტრაციო ფორმა, რომელიც განთავსებულია კურსის შიდა გვერდზე. კურსზე რეგისტრაციის დასრულების შემდეგ დაიწყება შემოსული განცხადებების გადარჩევა. - პირველ ეტაპზე საჭიროა, შეავსო სასურველი კურსისთვის განკუთვნილი სარეგისტრაციო ფორმა, რომელიც განთავსებულია კურსის შიდა გვერდზე. კურსზე რეგისტრაციის დასრულების შემდეგ დაიწყება შემოსული განცხადებების გადარჩევა. "
-
-     
-        this.questionContainer.appendChild(title);
-        this.questionContainer.appendChild(questionParent);
-
-        answerContainer.appendChild(answerHeader);
-        answerContainer.appendChild(answerSectionText);
-        answerContainer.appendChild(answerFooter);
-        questionParent.addEventListener("click",()=>{
-            if(svgContainer.innerHTML==svgCodeDown){
-                svgContainer.innerHTML=svgCodUp;
-                this.questionContainer.appendChild(answerContainer)
-            }else{
-                svgContainer.innerHTML=svgCodeDown;
-                this.questionContainer.removeChild(answerContainer)
-
+        
+        
+        let headers = document.querySelectorAll('.headers');
+        let svgs = document.querySelectorAll('.svg-cont');
+        
+        console.log(headers)
+        
+        
+        for (var i=0; i<headers.length; i++) {
+                svgs[i].innerHTML=svgCodeDown;
+        
+                headers[i].addEventListener('click', toggleDisplay);
             }
-        })
-        questionParent.appendChild(question);
-        questionParent.appendChild(svgContainer);
-        svgContainer.innerHTML=svgCodeDown;
-
-     
-
+        
+        function toggleDisplay() {
+            if(this.childNodes[3].innerHTML==svgCodeDown){
+            this.childNodes[3].innerHTML=svgCodUp
+        }else{
+            this.childNodes[3].innerHTML=svgCodeDown
+        }
+            if (this.parentNode.classList.contains('current-item')) {
+        
+        
+                var currentlyDisplayed = document.querySelectorAll('.current-item');
+         
+        
+        
+                for (var e=0; e<currentlyDisplayed.length; e++) {
+        
+                    currentlyDisplayed[e].classList.remove('current-item');
+                    svgs[e].innerHTML=svgCodeDown;
+                }
+        
+            } else {
+           
+        
+                this.closest('.item').classList.add('current-item');
+        
+            }
+        }
 
     }
 
@@ -96,3 +100,5 @@ const Questions={
 
 Courses.renderCourses();
 Questions.renderQuestions();
+
+
